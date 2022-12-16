b70da8265d2a01f9f8d951fb3341e54ba8813d3c
slebetman <slebetman@gmail.com>
2022-12-16 08:46:14 +0800

    Refactored to be more modular and added fonts

--- font-canvas.js
+++ font-canvas.js
@@ -1,24 +1,67 @@
-import {get} from "./lib/util.js"
-import {getFontMetrics, generateCharScale} from "./lib/canvaslib.js"
+import {get, div} from "./lib/util.js"
+import {generateCharScale, getMetricsForFont} from "./lib/canvaslib.js"
 
-let testFont = 'JMH Typewriter Bold';
+const fonts = [
+	// 'Carbon',
+	'CMU',
+	'Erika Typewriter',
+	'Erika Typewriter Bold',
+	'Droid Sans Mono',
+	'JMH Typewriter',
+	'JMH Typewriter Bold',
+	'Underwood Champion',
+	'Nouveau IBM',
+	'PT Mono',
+	'Iconsolata',
+	'Iconsolata Bold',
+	'SV Basic',
+	'SV Basic Bold',
+	// 'Microscan',
+	'DEC Terminal',
+	// 'Cutive Mono',
+];
+
+let fontStats = [];
 
 window.onload = async () => {
 	const paper = get('#paper');	
 	const ctx = paper.getContext('2d',{ willReadFrequently: true });
 
 	let log = get('#message');
-	log.innerHTML = 'Scanning font..';
-	
-	let charscale = await generateCharScale(ctx, testFont);
-	
-	let fontHeight = getFontMetrics(ctx,'M').height;
-	ctx.fillText(charscale, 0, fontHeight);
-	console.log(charscale);
+	log.innerHTML = 'Scanning fonts..';
+
+	for (let theFont of fonts) {
+
+		let charscale = (await generateCharScale(ctx, theFont)).join('');
+		
+		document.body.appendChild(div({},[
+			div({
+				style: {
+					font: `20px "${theFont}"`,
+					marginTop: '15px',
+				}
+			}, `${theFont} (${charscale.length} levels)`),
+			div({
+				style: {
+					font: `15px "${theFont}"`
+				}
+			}, charscale)
+		]))
+
+		let metrics = await getMetricsForFont(ctx, theFont, 15);
+
+		fontStats.push({
+			font: theFont,
+			metrics,
+			charscale
+		})
+			
+		console.log(charscale);
+	}
 	
 	log.innerHTML = '';
+	console.log('font stats', fontStats);
 	console.log('done');
-	
 }
 
 console.log('waiting for load');
